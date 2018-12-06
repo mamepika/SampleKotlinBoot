de16f250ed1f3823e6f2a0d5ee49ec11aada4d4c
tatsushisato <starlightprayer@gmail.com>
2018-12-06 17:21:42 +0900

    add message

--- src/main/kotlin/com/example/SampleKotlinBoot/controller/SampleController.kt
+++ src/main/kotlin/com/example/SampleKotlinBoot/controller/SampleController.kt
@@ -1,6 +1,7 @@
 package com.example.SampleKotlinBoot.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,5 +10,10 @@ class SampleController {
     @GetMapping("/hello")
     fun hello():String{
         return "Hello Kotlin !!"
+    }
+
+    @GetMapping("hello/{message}")
+    fun hello(@PathVariable message:String):String{
+        return "Hello Kotlin $message"
     }
 }
